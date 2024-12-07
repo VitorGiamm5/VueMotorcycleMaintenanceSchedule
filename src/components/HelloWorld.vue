20c81838eabb813e38591f7d28d6ad3502f96b61
Giamm <vitorgiammella+git@gmail.com>
2024-12-07 17:11:52 -0300

    new check point

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,52 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-  subMsg: string
-}>()
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+
+const increaseCount = () => {
+    count.value++;
+
+    if (count.value === 3) {
+        toast.add({ severity: "success", summary: "PrimeVue", detail: "Welcome to PrimeVue + Create Vue", life: 3000 });
+
+        count.value = 0;
+    }
+};
+
+const count = ref(0);
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      {{ subMsg }}
-    </h3>
-  </div>
+    <div class="greetings">
+        <Toast />
+
+        <h3>
+            <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+            <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a> +
+            <a href="https://primevue.org/" target="_blank" rel="noopener">PrimeVue</a>.
+        </h3>
+
+        <Button @click="increaseCount" label="Count"></Button>
+        <h5 class="green">{{ count }}</h5>
+    </div>
 </template>
 
 <style scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
 }
 
 h3 {
-  font-size: 1.2rem;
+    font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
+.green {
+    margin-top: 20px;
 }
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+button {
+    margin-top: 20px;
 }
 </style>
--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,14 +1,51 @@
 <script setup lang="ts">
-// import WelcomeItem from './WelcomeItem.vue'
-// import DocumentationIcon from './icons/IconDocumentation.vue'
-// import ToolingIcon from './icons/IconTooling.vue'
-// import EcosystemIcon from './icons/IconEcosystem.vue'
-// import CommunityIcon from './icons/IconCommunity.vue'
-// import SupportIcon from './icons/IconSupport.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { format } from 'date-fns';
+
+const scheduleData = ref([]);
+const loading = ref(true);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:9103/api/schedule');
+    scheduleData.value = response.data.data;
+    loading.value = false;
+   
+  } catch (error) {
+    console.error('Error fetching schedule data:', error);
+    loading.value = false;
+  }
+});
+
+const formatDate = (dateString) => {
+  return format(new Date(dateString), 'dd/MM/yyyy HH:mm');
+};
+
 </script>
 
 <template>
   <div class="schedule">
-    <p>olha schedule</p>
+    <ProgressSpinner v-if="loading" />
+    <DataTable v-else :value="scheduleData" tableStyle="min-width: 150rem">
+      <Column field="id" header="ID"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="email" header="Email"></Column>
+      <Column field="phone" header="Phone"></Column>
+      <Column field="phoneDDD" header="Phone DDD"></Column>
+      <Column field="observation" header="Observation"></Column>
+      <Column field="status" header="Status"></Column>
+      <Column field="motorcycleId" header="Motorcycle ID"></Column>
+      <Column field="startBusinessHour" header="Start Business Hour" :body="(data) => formatDate(data.startBusinessHour)"></Column>
+      <Column field="endBusinessHour" header="End Business Hour" :body="(data) => formatDate(data.endBusinessHour)"></Column>
+      <Column field="scheduleDate" header="Schedule Date" :body="(data) => formatDate(data.scheduleDate)"></Column>
+      <Column field="dateCreated" header="Date Created" :body="(data) => formatDate(data.dateCreated)"></Column>
+      <Column field="createdBy" header="Created By"></Column>
+      <Column field="dateLastUpdate" header="Date Last Update" :body="(data) => formatDate(data.dateLastUpdate)"></Column>
+    </DataTable>
   </div>
 </template>
+
+<style>
+
+</style>
